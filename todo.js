1df54cda53ded442bf828a08be5df267cec7ccb3
syedmuzammil037 <muzammil037.syed@gmail.com>
2021-05-27 20:38:01 +0530

    sceound commit

---
+++ todo.js
@@ -0,0 +1,43 @@
+function addTask() {
+    var input = document.getElementById("input");
+    // get current text from input field
+    var newTask = input.value;
+    // only add new item to list if some text was entered
+    if (newTask != "") {
+      // create new HTML list item
+      var item = document.createElement("li");
+      // add HTML for buttons and new task text
+      // Note, need to use '' because of "" in HTML
+   item.innerHTML = '<input type="button" class="done" onclick="markDone(this.parentNode)"  value="&#x2713;" /> ' + '<input type="button" class="remove" onclick="remove(this.parentNode)" value="&#x2715;" /> ' + newTask;
+      // add new item as part of existing list
+      document.getElementById("tasks").appendChild(item);  
+      
+       /* Step 4 below here */
+      
+    }
+  }
+  
+  
+  
+  // change styling used for given item
+  function markDone (item) { 
+      item.className = 'finished';
+  }
+  
+  /* Step 7 below here */
+  function remove (item) {
+    // remove item completely from document
+         item.remove();
+  }
+  
+  /* Step 11 below here */
+  function doAbout(d) {
+    
+    d.className='aboutcolor';
+  }
+  
+  /* Step 14 below here */
+  function clearAbout() {
+    
+  }
+  
